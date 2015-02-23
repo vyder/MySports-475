f0d668018158bbb0abba59623319314ffc19a06b
Vidur Murali <vidur.murali@gmail.com>
2015-02-23 12:59:02 +0530

    Write logs to file. Ignore local bundler config.

--- app.rb
+++ app.rb
@@ -15,6 +15,11 @@ enable :sessions
 
 configure do
   Mongoid.load!("./mongoid.yml", :development)
+  # logging is enabled by default in classic style applications,
+  # so `enable :logging` is not needed
+  file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+  file.sync = true
+  use Rack::CommonLogger, file
 end
 
 class NflTeam
